adc87a5ce041515e64611fb10e7cc7a53f569241
nicoFdez <nicolafe@ucm.es>
2022-05-11 18:33:42 +0200

    arreglo data check

--- .github/scripts/checkDataLocation.sh
+++ .github/scripts/checkDataLocation.sh
@@ -1,6 +1,6 @@
 mistake=0
 
-find . -type f -name -name *.json -or -name *.txt -or -name *.xaml -or -name *.xml -or -name *.csv | while read file
+find . -type f -name *.json -or -name *.txt -or -name *.xaml -or -name *.xml -or -name *.csv | while read file
 do
 	if [[ $file != ./Assets/Data/* ]] 
 	then 
