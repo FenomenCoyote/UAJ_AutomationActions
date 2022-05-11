831dffd5915074709bd6f6848b58f2eeb69adcb4
MarcoIMH <47715985+MarcoIMH@users.noreply.github.com>
2022-05-11 14:14:51 +0200

    ImagesLocations vm so

---
+++ .github/scripts/chImagesLocation.sh
@@ -0,0 +1,10 @@
+find . -type f -name *.png -or -name *.jpg -or -name *.jpeg | while read file
+do
+	if [[$file!=./Assets/Images/*]]
+	then
+		echo "ChImagesLocations: wrong file location at: $file"
+		exit 1
+	fi
+done
+echo "ChImagesLocations: success"
+exit 0
