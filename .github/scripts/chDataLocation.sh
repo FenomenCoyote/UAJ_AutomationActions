19678fce63788909c14f493719dcc49f3945de22
nicoFdez <nicolafe@ucm.es>
2022-05-12 13:28:43 +0200

    chData ahora solo en assets + limpieza scripts sh
    Co-Authored-By: MarcoIMH <47715985+MarcoIMH@users.noreply.github.com>

--- .github/scripts/chDataLocation.sh
+++ .github/scripts/chDataLocation.sh
@@ -1,12 +1,13 @@
 myvar=0
+cd Assets
 while read file
 do
-	if [[ $file != ./Assets/Data/* ]]
+	if [[ $file != ./Data/* ]]
 	then
 		myvar=1
 		echo "ChDataLocation: $file is not in Data folder"
 	fi
-done <<< "$(find . -type f -name *.json -or -name *.txt -or -name *.xaml -or -name *.xml -or -name *.csv)"
+done <<< "$(find . -type f -name *.json -or -name *.txt -or -name *.xaml -or -name *.csv)"
 if [ $myvar == 0 ]
 then
 	echo "ChDataLocation: success"
--- .github/scripts/chFilenameDuplicated.sh
+++ .github/scripts/chFilenameDuplicated.sh
@@ -1,11 +1,13 @@
-find ./Assets/ -type f -not -name *.meta -exec bash -c 'basename "$0" ".${0##*.*}"' {} \; | sort | uniq --repeated > tmpfile
-if [ -s tmpfile ]
-then
-	echo "ChFilenameDuplicated: Repeated files "
+find Assets/ -type f -not -name *.meta -exec bash -c 'basename "$0" ".${0##*.*}"' {} \; | sort | uniq --repeated > tmpfile
+
+if [ -s tmpfile ]; then
+	echo "Repeated Files: "
 	cat tmpfile
 	rm -f tmpfile
 	exit 1
-else
-	echo "ChFilenameDuplicated: success"
+else 
+	echo "All is good"
+	rm -f tmpfile
 fi
+
 exit 0
